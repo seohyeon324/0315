d8a52ed90b84849a2f919259a71d576c54796086
seohyeon324 <162652963+seohyeon324@users.noreply.github.com>
2024-03-15 11:32:26 +0900

    Add files via upload

---
+++ 240315.py
@@ -0,0 +1,34 @@
+print("Hello world!")
+print("Hello  world!")
+print("Hello","World")
+print("Hello"," ","World")
+
+print("오늘은 파이썬 챌린지 3일차 입니다")
+print('디버깅', "+", '연습중입니다')
+print('내일은'+'금요일' + '입니다')
+print("그래서 챌린지가 없습니다")
+
+print("나는"+"김서현"+"입니다")
+
+print("나는 김서현 입니다")
+
+print(len("김서현"))
+
+name="김서현"
+print(name)
+length=len(name)
+print(length)
+
+a="cat"
+b="dog"
+
+c=a
+a=b
+b=c
+
+print("a = "+ a)
+print("b = "+ b)
+
+영화="베테랑"
+배우="황정민"
+print("내가 좋아하는 영화는" + " " + 영화 + "이고 좋아하는 배우는" + " " + 배우 +"이다")
